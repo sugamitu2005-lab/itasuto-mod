da95a2acbe1a25c57608826564ea9ac166490d16
sugamitu2005-lab <sugamitu2005@gmail.com>
2026-03-05 23:23:36 +0900

    Update ItastMod.java

--- ItastMod/src/main/java/com/example/ItastMod.java
+++ ItastMod/src/main/java/com/example/ItastMod.java
@@ -1,1 +1,10 @@
-
+// src/main/java/com/example/itast/ItastMod.java
+@Mod.EventBusSubscriber(modid = "itast")
+public class ItastMod {
+    
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        // プレイヤーが止まっているか、移動中かなどを判定して
+        // 盤面処理を呼び出すエントリーポイントになります
+    }
+}
